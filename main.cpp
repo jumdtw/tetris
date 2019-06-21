c2a271e3a9c1dbb6599793b2037018f6c1ca5874
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2019-06-21 15:47:20 +0900

    define MINO shape and angle

--- main.cpp
+++ main.cpp
@@ -7,14 +7,37 @@
 using namespace std;
 
 char field[FIELD_HEIGHT][FIELD_WIDTH];
+//すべてのミノが４つのブロックで構成されていることを利用する。
+char mino[MINO_NUM][MINO_ALL_ANGLE][4][4];
 
-int main(){
+void init_field(){
     memset(field,0,sizeof(field));
     for(int i=0;i<FIELD_HEIGHT;i++){
         field[i][0] = 1;
+        field[i][FIELD_WIDTH-1] = 1;
     }
+    for(int i=0;i<FIELD_WIDTH;i++){
+        field[FIELD_HEIGHT-1][i] = 1;
+    }
+}
+
+void update(){
+    for(int i=0;i<FIELD_HEIGHT;i++){
+        for(int k=0;k<FIELD_WIDTH;k++){
+            if(field[i][k]==1){
+                cout << "W";
+            }else{
+                cout << "S";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main(){
+    init_field();
+    //updateで描画もおこなう
     for(;;){
-        printf("a\n");
         if(kbhit()!=0){
             char ch = getch();
             if(ch=='D'){return 0;}
--- main.hpp
+++ main.hpp
@@ -4,4 +4,23 @@
 #define FIELD_WIDTH 12
 #define FIELD_HEIGHT 22
 
+enum MINO{
+    MINO_T,
+    MINO_I,
+    MINO_O,
+    MINO_L,
+    MINO_J,
+    MINO_S,
+    MINO_Z,
+    MINO_NUM,
+};
+
+enum ANGLE{
+    MINO_0,
+    MINO_90,
+    MINO_180,
+    MINO_270,
+    MINO_ALL_ANGLE,
+};
+
 #endif
